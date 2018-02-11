d5b3826ce373846eebc7c92ef3d7b2868f44671d
Sadat Mirza <sadatmirza0@gmail.com>
2018-02-11 16:37:11 +0530

    Implementing Adapter 1

--- Downloads/Ud-Scr-And/Android basic DB builds/MyMovies/app/src/main/java/com/example/android/mymovies/MainActivity.java
+++ Downloads/Ud-Scr-And/Android basic DB builds/MyMovies/app/src/main/java/com/example/android/mymovies/MainActivity.java
@@ -8,9 +8,14 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.List;
 import com.example.android.mymovies.NetworkUtils;
 
@@ -20,13 +25,8 @@ import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mSearchResultsTextView;
-
-    MovieAdapter adapter;
-
-    static String S1 = "https://api.themoviedb.org/3/movie/popular?api_key=e6119fc0e6963d6ee2300a97c6d1cf22";
-
     public static URL githubSearchUrl = null;
+    static String S1 = "https://api.themoviedb.org/3/movie/popular?api_key=e6119fc0e6963d6ee2300a97c6d1cf22";
 
     static {
         try {
@@ -36,31 +36,39 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    MovieAdapter adapter;
+    private TextView mSearchResultsTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mSearchResultsTextView = (TextView) (findViewById(R.id.text_view));
-        new GithubQueryTask().execute(githubSearchUrl);
+        new GithubQueryTask().execute();
     }
 
     private void makeGithubSearchQuery() {
     }
 
-    public class GithubQueryTask extends AsyncTask<URL, Void, String> {
+    public class GithubQueryTask extends AsyncTask<Void, Void, Movie[]> {
 
         private final String LOG_TAG = GithubQueryTask.class.getSimpleName();
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+        // Will contain the raw JSON response as a string.
+        String movieinfo = null;
 
         protected void onPostExecute(Movie[] singleMovie) {
             if (singleMovie != null) {
-                adapter.clear();
+                //adapter.clear();
                 for (int i = 0; i < singleMovie.length; i++) {
                     Movie oneMovie = singleMovie[i];
                     Log.v(LOG_TAG, oneMovie.movieTitle + oneMovie.movieImage);
-                    adapter.add(oneMovie);
+                    //adapter.add(oneMovie);
+
                 }
             }
-            super.onPostExecute(Movie);
+
         }
 
         private Movie[] getmovieData(String movieInfo)
@@ -82,30 +90,65 @@ public class MainActivity extends AppCompatActivity {
             return movieDetails;
         }
 
-
         // COMPLETED (2) Override the doInBackground method to perform the query. Return the results. (Hint: You've already written the code to perform the query)
         @Override
-        protected String doInBackground(URL... params){
-            URL searchUrl = params[0];
-            String githubSearchResults = null;
+        protected Movie[] doInBackground(Void... params) {
             try {
-                githubSearchResults = NetworkUtils.getResponseFromHttpUrl(searchUrl);
+
+                URL url = new URL("https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=e6119fc0e6963d6ee2300a97c6d1cf22");
+                String movieDbUrl = url.toString();
+                Log.v(LOG_TAG, movieDbUrl);
+                // Create the request to OpenWeatherMap, and open the connection
+                URLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuilder buffer = new StringBuilder();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    return null;
+                }
+                movieinfo = buffer.toString();
+                Log.v(LOG_TAG, movieinfo);
             } catch (IOException e) {
+                Log.e("PlaceholderFragment", "Error ", e);
+                // If the code didn't successfully get the weather data, there's no point in attemping
+                // to parse it.
+                return null;
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e("PlaceholderFragment", "Error closing stream", e);
+                    }
+                }
+            }
+            try {
+                return getmovieData(movieinfo);
+            } catch (JSONException e) {
                 e.printStackTrace();
             }
-            return githubSearchResults;
-        }
-
-
-        // COMPLETED (3) Override onPostExecute to display the results in the TextView
-        @Override
-        protected void onPostExecute(String githubSearchResults) {
-            if (githubSearchResults != null && !githubSearchResults.equals("")) {
-                mSearchResultsTextView.setText(githubSearchResults);
-            }
+            return null;
         }
     }
-
-
 }
 
--- Downloads/Ud-Scr-And/Android basic DB builds/MyMovies/app/src/main/java/com/example/android/mymovies/MovieAdapter.java
+++ Downloads/Ud-Scr-And/Android basic DB builds/MyMovies/app/src/main/java/com/example/android/mymovies/MovieAdapter.java
@@ -16,19 +16,20 @@ import java.util.List;
  * Created by sadat on 2/11/18.
  */
 
-public class MovieAdapter extends ArrayAdapter<Movie> {
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        Movie singleMoviecontent = getItem(position);
-        View rootView = LayoutInflater.from(getContext()).inflate(R.layout.grid_item, parent, false);
-        ImageView poster = (ImageView) rootView.findViewById(R.id.movie_poster_image);
-        Picasso.with(getContext()).load(singleMoviecontent.movieImage).into(poster);
-        TextView name = (TextView) rootView.findViewById(R.id.movie_name);
-        name.setText(singleMoviecontent.movieTitle);
-        return rootView;
-    }
-
-    public MovieAdapter(FragmentActivity context, List<Movie> resource) {
-        super(context, 0, resource);
-    }
+public class MovieAdapter {
+////        extends ArrayAdapter<Movie> {
+////    @Override
+////    public View getView(int position, View convertView, ViewGroup parent) {
+//////        Movie singleMoviecontent = getItem(position);
+//////        View rootView = LayoutInflater.from(getContext()).inflate(R.layout.grid_item, parent, false);
+//////        ImageView poster = (ImageView) rootView.findViewById(R.id.movie_poster_image);
+//////        Picasso.with(getContext()).load(singleMoviecontent.movieImage).into(poster);
+//////        TextView name = (TextView) rootView.findViewById(R.id.movie_name);
+//////        name.setText(singleMoviecontent.movieTitle);
+//////        return rootView;
+//    }
+//
+//    public MovieAdapter(FragmentActivity context, List<Movie> resource) {
+//        super(context, 0, resource);
+//    }
 }
