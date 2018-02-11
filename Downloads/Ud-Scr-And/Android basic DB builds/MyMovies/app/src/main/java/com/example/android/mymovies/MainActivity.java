40994716301be330c53aeffd285bb1a9157bd856
Sadat Mirza <sadatmirza0@gmail.com>
2018-02-11 21:50:09 +0530

    Added Recycler view in MovieAdapter

--- Downloads/Ud-Scr-And/Android basic DB builds/MyMovies/app/src/main/java/com/example/android/mymovies/MainActivity.java
+++ Downloads/Ud-Scr-And/Android basic DB builds/MyMovies/app/src/main/java/com/example/android/mymovies/MainActivity.java
@@ -27,25 +27,12 @@ import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static URL githubSearchUrl = null;
-    static String S1 = "https://api.themoviedb.org/3/movie/popular?api_key=e6119fc0e6963d6ee2300a97c6d1cf22";
-    static {
-        try {
-            githubSearchUrl = new URL(S1);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-    }
-    MovieAdapter adapter;
-    Context context;
-    private TextView mSearchResultsTextView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mSearchResultsTextView = (TextView) (findViewById(R.id.text_view));
+        GetMovieTask task = new GetMovieTask();
+        task.execute();
     }
-
 }
 
--- Downloads/Ud-Scr-And/Android basic DB builds/MyMovies/app/src/main/java/com/example/android/mymovies/MovieAdapter.java
+++ Downloads/Ud-Scr-And/Android basic DB builds/MyMovies/app/src/main/java/com/example/android/mymovies/MovieAdapter.java
@@ -1,34 +1,61 @@
 package com.example.android.mymovies;
 
-import android.content.ComponentName;
 import android.content.Context;
-import android.support.v4.app.FragmentActivity;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.ImageView;
-import com.squareup.picasso.Picasso;
-
-import java.util.List;
 
 /**
  * Created by sadat on 2/11/18.
  */
 
-public class MovieAdapter extends ArrayAdapter<Movie> {
-    public MovieAdapter(ComponentName context, List<Movie> resource) {
-        super(context, 0, resource);
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHolder> {
+
+    private String[] mMovieData;
+
+    public MovieAdapter() {
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        Movie singleMoviecontent = getItem(position);
-        View rootView = LayoutInflater.from(getContext()).inflate(R.layout.grid_item, parent, false);
-        ImageView poster = (ImageView) rootView.findViewById(R.id.image_item);
-        Picasso.with(getContext()).load(singleMoviecontent.movieImage).into(poster);
-        //TextView name = (TextView) rootView.findViewById(R.id.movie_name);
-        //name.setText(singleMoviecontent.movieTitle);
-        return rootView;
+    public MovieViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        Context context = viewGroup.getContext();
+        int layoutIDForListItem = R.layout.grid_item;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        boolean shouldAttachToParentImmediately = false;
+        View view = inflater.inflate(layoutIDForListItem, viewGroup, shouldAttachToParentImmediately);
+        return new MovieViewHolder(view);
     }
+
+    @Override
+    public void onBindViewHolder(MovieViewHolder movieAdapterViewHolder, int position) {
+        String MovieForThisDay = mMovieData[position];
+        movieAdapterViewHolder.mImageView.setImageResource(Integer.parseInt(MovieForThisDay));
+    }
+
+    @Override
+    public int getItemCount() {
+        if (mMovieData == null) return 0;
+        return mMovieData.length;
+    }
+
+    public void setMovieData(String[] movieData) {
+        mMovieData = movieData;
+        notifyDataSetChanged();
+    }
+
+    public class MovieViewHolder extends RecyclerView.ViewHolder {
+
+        public final ImageView mImageView;
+
+        public MovieViewHolder(View view) {
+            super(view);
+            mImageView = (ImageView) (view.findViewById(R.id.image_item));
+        }
+/*        void bind(int gridIndex){
+            mImageView.setId(Integer.parseInt(String.valueOf(gridIndex)));
+        }*/
+    }
+
 }
